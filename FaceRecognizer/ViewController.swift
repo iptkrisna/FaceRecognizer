31a300c93337914c4c8c580ba919f5bbad5c68ef
kulitbitu <krisnamath9@gmail.com>
2019-05-20 20:30:20 +0700

    Initial Commit

---
+++ FaceRecognizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceRecognizer
+//
+//  Created by I Putu Krisna on 20/05/19.
+//  Copyright © 2019 I Putu Krisna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
